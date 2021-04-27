f4a76343c52f6a27fa8104da28191f9234709cee
Rudraksh-coder <83273548+Rudraksh-coder@users.noreply.github.com>
2021-04-27 13:54:09 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -14,7 +14,7 @@ var gameOver, restart;
 var jumpSound , checkPointSound, dieSound
 
 function preload(){
-  trex_running = loadAnimation("trex1.png","trex3.png","trex4.png");
+  trex_running = loadAnimation("trex1.png","trex2.png","trex3.png");
   trex_collided = loadAnimation("trex_collided.png");
   
   groundImage = loadImage("ground2.png");
